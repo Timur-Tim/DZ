d8bb4a210574967940614b5a0e3aa04c8d129ca3
Timur <Timur._M@mail.ru>
2022-11-05 21:46:47 +0300

    task 2 complete

--- Dz5/Program.cs
+++ Dz5/Program.cs
@@ -1,37 +1,39 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-Console.WriteLine("Введите размер массива");
-int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите размер массива");
+// int size = Convert.ToInt32(Console.ReadLine());
 
-int[] CreateFillArray(int size)
-{
-    int[] array = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        array[i] = new Random().Next(100, 1000);
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
-    return array;
-}
+// int[] CreateFillArray(int size)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(100, 1000);
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+//     return array;
+// }
 
-void HonestArray(int[] numbers)
-{
-    
-    int a = 0;
-    for (int z = 0; z < numbers.Length; z++)
-    {
-        if (numbers [z] % 2 == 0) a++;
-    }
-    Console.Write($"Всего чётных чисел {a}");
-} 
+// void HonestArray(int[] numbers)
+// {
+//     int a = 0;
+//     for (int z = 0; z < numbers.Length; z++)
+//     {
+//         if (numbers [z] % 2 == 0) a++;
+//     }
+//     Console.Write($"Всего чётных чисел {a}");
+// } 
 
-int[] numbers = CreateFillArray(size);
-HonestArray(numbers);
+// int[] numbers = CreateFillArray(size);
+// HonestArray(numbers);
 
 // int[] mas = CreateFillArray();
 // int[] res = HonestArray();
+
+
+
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
@@ -39,9 +41,11 @@ HonestArray(numbers);
 
 // Console.WriteLine("Введите размер массива");
 // int size = Convert.ToInt32(Console.ReadLine());
-// int[] numbers = new int[size];
+// // int[] array = new int[size];
 
-// int[] CreateFillArray()
+
+// int[] FillArray(int size)
+// // создание массива
 // {
 //     int[] array = new int[size];
 //     for (int i = 0; i < size; i++)
@@ -52,6 +56,25 @@ HonestArray(numbers);
 //     Console.WriteLine();
 //     return array;
 // }
-// int[] mas = CreateFillArray();
+// //нахождение четных элементов 
+
+// int EvenArray(int[] array)
+// {
+//     int x = 0;
+//     // for(int a = 0; a < array.Length; a++);
+//     // {
+//         for (int i = 0; i < array.Length; i = i + 2)
+//         {
+//           x = x + array[i];  //(array[i] % 2 == 1)
+//         }
+//         Console.WriteLine(x);
+//     return x;
+    
+// }
+// int[] numbers = FillArray(size);
+// EvenArray(numbers);
+
+
 
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+

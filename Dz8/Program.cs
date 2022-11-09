a8a7c4f9ae8f29455e0030e7605ce51ea2e00977
Timur <Timur._M@mail.ru>
2022-11-09 21:09:43 +0300

    task 1 complete

--- Dz8/Program.cs
+++ Dz8/Program.cs
@@ -7,6 +7,8 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+
 Console.WriteLine("Введите количество строк");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите количество столбцов");
@@ -39,24 +41,28 @@ void PrintArray(int[,] image)
 
 void Sorted (int[,] arr)
 {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-        int max = 0;    
-        for (int i = 0; i < array.GetLength(0); i++)
+    for (int j = 0; j < array.GetLength(0); j++)
+    {            
+        for (int i = 0; i < array.GetLength(1); i++)
         {
-            if(i > max) i = max;
-            
-        }               
-        result = sum / array.GetLength(0);
-        
-        Console.Write("{0:F2}", result + ";   ");
-    }
-   
+             for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }         
+    }   
 }
 Console.WriteLine();
 int[,] mas = FillArray(m, n);
 PrintArray(mas);
-Sorted(array);
+Sorted(mas);
+Console.WriteLine();
+PrintArray(mas);
 
 
 // Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.

3da732f849f9195fc29fa93a5ffd55586b0b2ca7
Timur <Timur._M@mail.ru>
2022-11-10 16:44:59 +0300

    task 4 complete

--- Dz8/Program.cs
+++ Dz8/Program.cs
@@ -9,60 +9,60 @@
 // 8 4 4 2
 
 
-Console.WriteLine("Введите количество строк");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите количество столбцов");
-int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество строк");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-int[,] array = new int[m, n];
+// int[,] array = new int[m, n];
 
-int[,] FillArray(int m, int n)
-{
+// int[,] FillArray(int m, int n)
+// {
 
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-            array[i, j] = new Random().Next(1, 10);
-    }
-    return array;
-}
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             array[i, j] = new Random().Next(1, 10);
+//     }
+//     return array;
+// }
 
-void PrintArray(int[,] image)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{image[i, j]} \t");
-        }
-        Console.WriteLine();
-    }
-}
+// void PrintArray(int[,] image)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{image[i, j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
 
-void Sorted (int[,] arr)
-{
-    for (int j = 0; j < array.GetLength(0); j++)
-    {            
-        for (int i = 0; i < array.GetLength(1); i++)
-        {
-             for (int k = 0; k < array.GetLength(1) - 1; k++)
-            {
-                if (array[i, k] < array[i, k + 1])
-                {
-                    int temp = array[i, k + 1];
-                    array[i, k + 1] = array[i, k];
-                    array[i, k] = temp;
-                }
-            }
-        }         
-    }   
-}
-Console.WriteLine();
-int[,] mas = FillArray(m, n);
-PrintArray(mas);
-Sorted(mas);
-Console.WriteLine();
-PrintArray(mas);
+// void Sorted (int[,] arr)
+// {
+//     for (int j = 0; j < array.GetLength(0); j++)
+//     {            
+//         for (int i = 0; i < array.GetLength(1); i++)
+//         {
+//              for (int k = 0; k < array.GetLength(1) - 1; k++)
+//             {
+//                 if (array[i, k] < array[i, k + 1])
+//                 {
+//                     int temp = array[i, k + 1];
+//                     array[i, k + 1] = array[i, k];
+//                     array[i, k] = temp;
+//                 }
+//             }
+//         }         
+//     }   
+// }
+// Console.WriteLine();
+// int[,] mas = FillArray(m, n);
+// PrintArray(mas);
+// Sorted(mas);
+// Console.WriteLine();
+// PrintArray(mas);
 
 
 // Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
@@ -72,6 +72,64 @@ PrintArray(mas);
 // 8 4 2 4
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+// Console.WriteLine("Введите количество строк");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int[,] array = new int[m, n];
+
+// int[,] FillArray(int m, int n)
+// {
+
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             array[i, j] = new Random().Next(1, 10);
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[,] image)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{image[i, j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void SumMinElements(int[,] array)
+// {
+//     int minRow = 0;
+//     int minSumRow = 0;
+//     int sumRow = 0;
+//     for (int i = 0; i < array.GetLength(1); i++)
+//     {
+//         minRow += array[0, i];
+//     }
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++) sumRow += array[i, j];
+//         if (sumRow < minRow)
+//         {
+//             minRow = sumRow;
+//             minSumRow = i;
+//         }
+//         sumRow = 0;
+//     }
+//     Console.Write($"Cтрока с наименьшей суммой элементов: {minSumRow + 1}");
+// }
+
+// Console.WriteLine();
+// int[,] mas = FillArray(m, n);
+// PrintArray(mas);
+// SumMinElements(mas);
+
+
 
 
 
@@ -84,7 +142,6 @@ PrintArray(mas);
 // 15 18
 
 
-
 // Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 // Массив размером 2 x 2 x 2
 // 66(0,0,0) 25(0,1,0)
@@ -92,7 +149,42 @@ PrintArray(mas);
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
+int[,,] array3D = new int[2, 2, 2];
 
+void FillArray(int[,,] arr)
+{
+    int count = 10;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[k, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
+
+
+void PrintIndex(int[,,] arr)
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                Console.Write($"{array3D[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
+FillArray(array3D);
+PrintIndex(array3D);
 
 // Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
 // Например, на выходе получается вот такой массив:

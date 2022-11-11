1d79bff471b38b57668b4f79730b0f08788fc759
Timur <Timur._M@mail.ru>
2022-11-11 13:10:13 +0300

    task 3 complete

--- Dz9/Program.cs
+++ Dz9/Program.cs
@@ -18,20 +18,42 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-Console.Write("Введите целое число ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите целое число ");
-int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите целое число ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите целое число ");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-int SumRec(int m, int n)
-{
-    int sum = m;
-    if (m == n) return n;    
-    return SumRec(m, n-1) + n;
-}
-Console.WriteLine($" {SumRec(m, n)} ");
+// int SumRec(int m, int n)
+// {
+//     int sum = m;
+//     if (m == n) return n;    
+//     return SumRec(m, n-1) + n;
+// }
+// Console.WriteLine($" {SumRec(m, n)} ");
 
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Введите целое число ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int FunAkkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return FunAkkerman(m - 1, 1);
+    }
+    else
+    {
+        return (FunAkkerman(m - 1, FunAkkerman(m, n - 1)));
+    }
+}
+Console.WriteLine($" {FunAkkerman(m, n)} ");
